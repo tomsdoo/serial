a49bc13e7943d80479136fded19cb7959dd393dd
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-07-10 08:25:18 +0900

    number() parameter is not required (#10)
    * length parameter is not required
    
    * test code is changed

--- src/serial.ts
+++ src/serial.ts
@@ -9,7 +9,7 @@ class Chars {
   constructor(provided_start_char : string){
     this.start_char = provided_start_char;
   }
-  public getRange(length : number, start ?: number){
+  public getRange(length? : number, start ?: number){
     const lc : number = this.start_char.charCodeAt(0);
     return Serial.number(length, start).map(function(i:number){
       return String.fromCharCode((i % 26) + lc);
@@ -18,19 +18,20 @@ class Chars {
 }
 export default class Serial {
   constructor(){}
-  public static number(length:number, start ?: number){
+  public static number(length?: number, start ?: number){
     let r : number[] = [];
     let ls : number = start ? start : 0;
-    for(let i = 0; i < length; i++){
+    const localLength = length || 0;
+    for(let i = 0; i < localLength; i++){
       r.push(i + ls);
     }
     return r;
   }
   public static char = {
-    lower(length : number, start ?: number){
+    lower(length?: number, start ?: number){
       return (new Chars("a")).getRange(length, start);
     },
-    upper(length : number, start ?: number){
+    upper(length?: number, start ?: number){
       return (new Chars("A")).getRange(length, start);
     }
   };
