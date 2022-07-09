049711422f0257ee71668679b63a2978a6e83c0f
tom <toms@live.jo>
2022-07-10 08:05:58 +0900

    ts-node testing is aded

---
+++ test/test.ts
@@ -0,0 +1,57 @@
+import {describe, it } from "mocha";
+import Serial from "../src/serial";
+import { strict as assert } from "assert";
+
+describe("Serial.number()", () => {
+  it("returns 0 length array if parameter is nothing", () => {
+    assert.equal(Serial.number(0).length, 0);
+  });
+
+  it("returns array that has length as parameter provided", () => {
+    const len = 12;
+    assert.equal(Serial.number(len).length, len);
+  });
+
+  it("returns array that starts with second parameter", () => {
+    const start = 2, len = 10;
+    assert.equal(Serial.number(len, start)[0], start);
+  });
+});
+
+describe("Serial.char.lower()", () => {
+  it("returns 0 length array if parameter is nothing", () => {
+    assert.equal(Serial.char.lower(0).length, 0);
+  });
+
+  it("returns array that has length as parameter provided", () => {
+    const len = 12;
+    assert.equal(Serial.char.lower(len).length, len);
+  });
+
+  it("returns array that starts with second parameter", () => {
+    const start = 2, len = 10;
+    assert.equal(
+      Serial.char.lower(len, start)[0],
+      String.fromCharCode(("a").charCodeAt(0) + start)
+    );
+  });
+});
+
+describe("Serial.char.upper()", () => {
+  it("returns 0 length array if parameter is nothing", () => {
+    assert.equal(Serial.char.upper(0).length, 0);
+  });
+
+  it("returns array that has length as parameter provided", () => {
+    const len = 12;
+    assert.equal(Serial.char.upper(len).length, len);
+  });
+
+  it("returns array that starts with second parameter", () => {
+    const start = 2, len = 10;
+    assert.equal(
+      Serial.char.upper(len, start)[0],
+      String.fromCharCode(("A").charCodeAt(0) + start)
+    );
+  });
+});
